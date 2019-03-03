098a079f8fa7fe2ed80438a3eaef93e1b96e0fa8
IgorPystovit <igor.pystovit@gmail.com>
2019-03-03 16:36:07 +0200

    Added Lab1.cs Lab2.cs

---
+++ C#Labs/Lab1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Executor
+    {
+        static void Main(string[] args)
+        {
+            Lab1 lab = new Lab1();
+            lab.Scan();
+            lab.Calculation();
+            lab.Output();
+        }
+    }
+
+    class Lab1
+    {
+        private double x = 0.0;
+        private double result = 0.0;
+        private double y = 0.0;
+
+        //reading values of variables x,y from a keyboard
+        public void Scan()
+        {
+            Console.WriteLine("Input the value of x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input the value of y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+        }
+        //@return void
+
+        //Calculating result with given values
+        public void Calculation()
+        {
+            //introducing local
+            //double localCalc = (142 * (y - x)) / 16.32;
+            result = Math.Sqrt((x * y * y) + (y * Math.Sin(x)) + (142 * x * x * y)) + Math.Tan(x * y) - (142 * (y - x)) / 16.32;
+        }
+        //@return void
+
+        //Outputing result
+        public void Output()
+        {
+            Console.WriteLine("Result = " + result);
+        }
+        //@return void
+    }
+}
---
+++ C#Labs/Lab2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace RandomApp
+{
+    public class Lab2
+    {
+        public static void Apply(FunctionHandler fh)
+        {
+            fh.Executioner();
+        }
+        public static void Main(String[] args)
+        {
+            Apply(new Function1());
+        }
+    }
+
+    public abstract class FunctionHandler
+    {
+        public abstract void CalculateFunction();
+        public abstract void DataScanner();
+        public abstract void Executioner();
+        public void TabulateFunction(Object value)
+        {
+            Console.Write(value+"\t");
+        }
+    }
+
+    public class Function1 : FunctionHandler
+    {
+        private double ArgValue = 0.0;
+        private double Step = 0.0;
+        private double Border = 0.0;
+
+        public Function1() => DataScanner();
+
+        public override void DataScanner()
+        {
+            Console.WriteLine("Input value of argument");
+            ArgValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input value of step");
+            Step = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input value of borders");
+            Console.WriteLine("Border");
+            Border = Convert.ToDouble(Console.ReadLine());
+        }
+        public override void CalculateFunction()
+        {
+            for (;ArgValue < (Border + Step); ArgValue += Step)
+            {
+                TabulateFunction(SwitchFunction());
+            }
+        }
+
+        private double SwitchFunction()
+        {
+            double iTemp = 0.0;
+            if (ArgValue <= -0.7)
+            {
+                iTemp = Math.Atan(Math.Pow(ArgValue,3));
+            }
+            else if ((-0.7 < ArgValue) && (ArgValue <= -0.6))
+            {
+                iTemp = Math.Tan(ArgValue + Math.Log(Math.Abs(ArgValue)));
+            }
+            else if (ArgValue >= -0.6)
+            {
+                iTemp = 1 / Math.Tan(Math.Pow(ArgValue, 2));
+            }
+            else
+            {
+                iTemp = 0.0;
+            }
+            return iTemp;
+        }
+
+        public override void Executioner()
+        {
+            CalculateFunction();
+        }
+
+
+    }
+
+
+}
